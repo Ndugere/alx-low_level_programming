9dbbd1fcba034d9d85cb6781982d0ae5f95cd921
Ndugere <ndungudavid972@gmail.com>
2023-02-19 06:46:19 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - writing somthing to the output
+ * Return: 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
