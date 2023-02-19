4893a71e654468e4a0a64ca8d17843f55e13c0a8
Ndugere <ndungudavid972@gmail.com>
2023-02-19 14:28:07 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printing both upper and lower
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+	{
+		for (l > 'a'; l <= 'z'; l++)
+		{
+			putchar(l);
+		}
+		for (l > 'A'; l <= 'Z'; l++)
+		{
+			putchar(l);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
