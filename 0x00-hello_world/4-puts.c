c13340c258869704f22cc181a2886698c9f07bae
Ndugere <ndungudavid972@gmail.com>
2023-02-19 03:36:47 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Program rahisi
+ * Return:0
+ * inaffa kureturn zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"\n);
+	return (0);
+}
