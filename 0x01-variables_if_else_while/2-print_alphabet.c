89e166366204ebdc570674123a2fea4b71d7d058
Ndugere <ndungudavid972@gmail.com>
2023-02-19 13:28:48 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+/**
+ * main - lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char lc;
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+		{
+			putchar(lc);
+		}
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
+
+
