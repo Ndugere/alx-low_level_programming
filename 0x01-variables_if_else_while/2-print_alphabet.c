e77be5adc1f3aeae2a94141d231ba2f1ef9ae4e3
Ndugere <ndungudavid972@gmail.com>
2023-02-19 13:46:22 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,11 @@
 int main(void)
 {
 	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
-		for (lc = 'a'; lc <= 'z'; lc++)
-		{
-			putchar(lc);
-			putchar('\n');
-		}
+		putchar(lc);
 	}
+	putchar('\n');
 	return (0);
 }
-
-
