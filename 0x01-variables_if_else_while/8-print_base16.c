69197128b1789e879fcc4f5a68f86d22f8948c3f
Ndugere <ndungudavid972@gmail.com>
2023-02-20 07:15:09 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - base kumi na sita
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	char l;
+
+	for (a = 0; a < 10; a++)
+		putchar((a % 10) + '0');
+	for (l = 'a'; l <= 'f' l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
