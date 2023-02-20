3f97b8f0056048ba55e0e93bc8abccc3a748d2d7
Ndugere <ndungudavid972@gmail.com>
2023-02-20 06:03:46 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing numbers
+ * Return: 0
+ */
+Int main(void)
+{
+	int i;
+	{
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+		return (0);
+	}
+}
