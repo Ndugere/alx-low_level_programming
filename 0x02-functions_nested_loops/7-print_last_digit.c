80326dfd7cd06924d34f5b4b076b879b6f676974
Ndugere <ndungudavid972@gmail.com>
2023-02-22 04:19:57 -0800

    7- last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - heb print last digit
+ * Return: Hiyo namber ya mwisho
+ * @n: number tunachunguza
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last =  n % 10;
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
