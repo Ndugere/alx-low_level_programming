1e771cc690f00055aaf8dbc2fab3b2e2bfbcca28
Ndugere <ndungudavid972@gmail.com>
2023-02-22 06:35:02 -0800

    Hii nikuongeza tu

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - simeple addition
+ * Return: addition results
+ * int: is what will be added
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
