c40cb0490ee3318f7541326ce906540527aadca4
Ndugere <ndungudavid972@gmail.com>
2023-03-06 10:25:43 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+	int sum = 0;
+	int y = 1;
+	int n;
+
+	for (n = 0; n <= 50; n++)
+	{
+		sum = x + y;
+		printf("%d, ", sum);
+		x = y;
+		y = sum;
+	}
+	return (0);
+}
