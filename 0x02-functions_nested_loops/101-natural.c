67d5df318a53e056a28f109d5d0778f0756697ab
Ndugere <ndungudavid972@gmail.com>
2023-03-06 08:25:52 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,15 @@
  */
 int main(void)
 {
-	int i; int sum = 0;
+	int i;
+	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
-				{
-				sum = sum + i;
-				}
+		{
+			sum = sum + i;
+		}
 	}
 	printf("Total sum is equal to: %d\n", sum);
 
