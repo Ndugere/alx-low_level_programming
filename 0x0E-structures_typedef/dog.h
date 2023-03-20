8a12588179a098b4ed70142acabe1e30ede3928a
Ndugere <ndungudavid972@gmail.com>
2023-03-20 15:48:12 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,28 @@
-#include "dog.h"
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * init_dog - Program that initializes certain variables
- * @d: Struct for initializaiton
- * @name: Holds name char array
- * @age: Holds age float value
- * @owner: Holds owner name char array
- * Return: Nothing
+ * struct dog - A simple struct that contains variables
+ * @name: Holds name of dog in char array
+ * @age: Holds number, including any decimals
+ * @owner: Holds name of owner in char array
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
