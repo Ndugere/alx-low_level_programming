94843ac5286a44b0718825e223d8afd1175bdc1a
Ndugere <ndungudavid972@gmail.com>
2023-04-12 09:17:01 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * get_endianness - inarudisha endianness
+ *
+ * Return: 0 for big endian, lakini 1 for little endian
+ */
+int get_endianness(void)
+{
+	int y = 1;
+
+	return (*((char *) &y) + '0');
+}
